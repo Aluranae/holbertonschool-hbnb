29fb16f9c9d22b486ef949aaa94e9c66857cd715
ninaglss15 <10526@holbertonstudents.com>
2025-06-30 23:10:22 +0200

    Remove commented initialization lines for clarity in app setup

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -6,12 +6,9 @@ def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Initialise les extensions
+
     db.init_app(app)
     jwt.init_app(app)
 
-    # Import et registre les blueprints ici si nécessaire
-    # from .routes.main import main_bp
-    # app.register_blueprint(main_bp)
 
     return app
--- part3/app/extensions.py
+++ part3/app/extensions.py
@@ -2,7 +2,6 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 
-# Initialize extensions
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
