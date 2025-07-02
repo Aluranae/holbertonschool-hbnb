bfe173e4e37efd76ff5f76ef72b539ca2dc961e4
Alurae <10475@holbertonstudents.com>
2025-07-02 07:33:36 +0200

    Refactor app initialization and configuration to integrate JWT and SQLAlchemy extensions

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from config import DevelopmentConfig
+from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
+from .extensions import db, jwt
 
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
@@ -11,11 +15,14 @@ from app.api.v1.reviews import api as reviews_ns
 bcrypt = Bcrypt()
 
 
-def create_app():
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
+    app.config.from_object(config_class)
 
     # Initialisation de bcrypt avec l'app
     bcrypt.init_app(app)
+    db.init_app(app)
+    jwt.init_app(app)
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
     api.add_namespace(users_ns, path='/api/v1/users')
--- part3/config.py
+++ part3/config.py
@@ -1,16 +1,13 @@
-import os
-
-
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    DEBUG = False
+    SECRET_KEY = "your_secret_key"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
 
 
-config = {
-    'development': DevelopmentConfig,
-    'default': DevelopmentConfig
-}
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = "mysql://user:password@localhost/hbnb_prod"
